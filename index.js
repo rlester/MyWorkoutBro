c26e907b06e2a2ba046626bd62fa0c925592b54b
Rob Lester <rlester@mobispoke.com>
2017-06-28 00:19:50 -0400

    Added more unit testing, fixed bugs, cleaned up code

--- index.js
+++ index.js
@@ -12,64 +12,79 @@ const APP_ID = 'amzn1.ask.skill.1e4f454b-6824-44a1-9665-cf8e2a43b69f';
 
 const handlers = {
   'GetExerciseInfo': function() {
-      const user = this.event.session.user.userId,
-      userExercise = his.event.request.intent.slots.exercise.value,
+    const user = this.event.session.user.userId,
+      userExercise = this.event.request.intent.slots.exercise.value,
       normalizedExercise = normalizeExercise(userExercise);
 
     var item = this.attributes[uniqueIdentifier(user, normalizedExercise)];
 
     if (typeof item !== 'undefined') {
-      self.emit(':tell', tellAboutExercise(item.exercise, item.weight, item.reps));
+      this.emit(':tell', tellAboutExercise(item.exercise, item.weight, item.reps));
     } else {
-      self.emit(':tell', 'Your workout bro says do you even ' + userExercise + ' bro? You have\'t told me what you ' + userExercise );
+      this.emit(':tell', 'Workout bro says do you even ' + userExercise + ' bro? You have\'t told me what you ' + userExercise);
     }
   },
 
   'SetExerciseInfo': function() {
-    const self = this,
-      slots = this.event.request.intent.slots,
+    const slots = this.event.request.intent.slots,
       user = this.event.session.user.userId,
-      exercise = normalizeExercise(this.event.request.intent.slots.exercise.value);
+      userExercise = this.event.request.intent.slots.exercise.value,
+      normalizedExercise = normalizeExercise(userExercise);
 
-    this.attributes[uniqueIdentifier(user, exercise)] = {
-      exercise: exercise,
+    this.attributes[uniqueIdentifier(user, normalizedExercise)] = {
+      exercise: normalizedExercise,
       weight: slots.weight.value,
       reps: slots.reps.value
     };
 
-    self.emit(':tell', tellAboutExercise(exercise, slots.weight.value, slots.reps.value));
+    if (typeof normalizedExercise !== 'undefined') {
+      this.emit(':tell', tellAboutExercise(normalizedExercise, slots.weight.value, slots.reps.value));
+    } else {
+      this.emit(':tell', "Seriously broseidon, what is " + userExercise + " I haven't heard of that before. I'll take note and add that as an exercise soon bro.");
+      console.warn("Exercise not found we should add " + userExercise);
+    }
   },
-};
-
-exports.handler = function(event, context) {
-  const alexa = Alexa.handler(event, context);
-  alexa.APP_ID = APP_ID;
-  alexa.locale = 'en-US';
-  alexa.dynamoDBTableName = 'MyWorkoutBro';
-  alexa.registerHandlers(handlers);
-  alexa.execute();
 };
 
 /**
  * Helpers
  **/
-
-function normalizeExercise(exercise) {
+var normalizeExercise = function(exercise) {
   const list = fs.readFileSync(path.join(__dirname, 'exercises.txt')).toString().split('\n');
-  const fuse = new Fuse(list,  {});
+  const fuse = new Fuse(list, {});
 
   var index = fuse.search(exercise);
-  return list[index];
+  var normalizedExercise = list[index[0]];
+
+  console.info("Normalized " + exercise + " to " + normalizedExercise);
+
+  return normalizedExercise
 }
 
-function tellAboutExercise(exercise, weight, reps) {
-  if (typeof reps !== 'undefined') {
+var tellAboutExercise = function(exercise, weight, reps) {
+  if (typeof reps !== 'undefined' && typeof weight !== 'undefined') {
     return 'You ' + exercise + ' ' + weight + ' pounds for ' + reps + ' reps';
-  } else {
+  } else if (typeof weight !== 'undefined') {
     return 'You ' + exercise + ' ' + weight + ' pounds';
+  } else {
+    return 'You ' + exercise + ' ' + reps + ' reps';
   }
 }
 
-function uniqueIdentifier(user, exercise) {
+var uniqueIdentifier = function(user, exercise) {
   return user + ':' + exercise;
+}
+
+module.exports = {
+  handler: function(event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.locale = 'en-US';
+    alexa.dynamoDBTableName = 'MyWorkoutBro';
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+  },
+  normalizeExercise: normalizeExercise,
+  tellAboutExercise: tellAboutExercise,
+  uniqueIdentifier: uniqueIdentifier
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,33 +1,34 @@
-import { expect } from 'chai';
+var chai = require('chai')
+var index = require('../index')
 
-import {
-  tellAboutExercise,
-  uniqueIdentifier,
-  normalizeExercise
-} from '../src';
+console.log(index)
 
 describe('uniqueIdentifier', () => {
   it('should construct a unique identifier', () => {
-    expect(uniqueIdentifier('amzn1.ask.account.AFIAWKT5YFTE', 'squat')).to.equal('amzn1.ask.account.AFIAWKT5YFTE:squat');
+    chai.expect(index.uniqueIdentifier('amzn1.ask.account.AFIAWKT5YFTE', 'squat')).to.equal('amzn1.ask.account.AFIAWKT5YFTE:squat');
   });
 });
 
 describe('tellAboutExercise', () => {
   it('should construct a string with weight and reps', () => {
-    expect(tellAboutExercise('squat', '255', '12')).to.equal('You squat 255 pounds for 12 reps');
+    chai.expect(index.tellAboutExercise('squat', '255', '12')).to.equal('You squat 255 pounds for 12 reps');
   });
 
   it('should construct a string with a weight and no reps', () => {
-    expect(tellAboutExercise('squat', '255')).to.equal('You squat 255 pounds');
+    chai.expect(index.tellAboutExercise('squat', '255')).to.equal('You squat 255 pounds');
   });
 });
 
 describe('normalizeExercise', () => {
   it('should normalize squatted to squat', () => {
-    expect(normalizeExercise('squatted')).to.equal('squat');
+    chai.expect(index.normalizeExercise('squatted')).to.equal('squat');
   });
 
   it('should normalize bicep curled to bicep curl', () => {
-    expect(normalizeExercise('bicep curled')).to.equal('bicep curl');
+    chai.expect(index.normalizeExercise('bicep curled')).to.equal('bicep curl');
+  });
+
+  it('should normalize benched to bench press', () => {
+    chai.expect(index.normalizeExercise('benchedd')).to.equal('bench press');
   });
 });
